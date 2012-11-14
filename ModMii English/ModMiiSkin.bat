d24e847624ae1efbc199fb7a3be0d6565a979442
xflak40 <11338309+xflak@users.noreply.github.com>
2012-11-14 00:43:33 +0000

    6.2.1 Changelog:
    Bugfix for WiiFlow download.
    
    What was supposed to be my final ModMii update didn't even last a full day. As such, I dub this ModMii update version "Final For Now".

--- ModMii English/ModMiiSkin.bat
+++ ModMii English/ModMiiSkin.bat
@@ -31,7 +31,7 @@ Exit
 
 ::----------------------------------------------------------
 
-set currentversion=6.2.0
+set currentversion=6.2.1
 set currentversioncopy=%currentversion%
 set agreedversion=
 
