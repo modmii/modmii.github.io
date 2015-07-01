f41a833d2085a6b39305254a4485fe5895ae3b5a
xflak <xflak@fa821f5b-d0dd-421d-b210-aa18b36e8302>
2015-07-01 23:49:44 +0000

    6.3.6

--- trunk/ModMii English/ModMiiSkin.bat
+++ trunk/ModMii English/ModMiiSkin.bat
@@ -31,7 +31,7 @@ Exit
 
 ::----------------------------------------------------------
 
-set currentversion=6.3.5
+set currentversion=6.3.6
 set currentversioncopy=%currentversion%
 set agreedversion=
 
