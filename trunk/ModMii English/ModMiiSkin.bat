20460e6aac2d6b0ee2792e3f1d2cf719ec5efc36
xflak <xflak@fa821f5b-d0dd-421d-b210-aa18b36e8302>
2018-01-23 05:12:10 +0000

    6.4.3: Fixed UnRAR links and parsing of neek2o versions available online.

--- trunk/ModMii English/ModMiiSkin.bat
+++ trunk/ModMii English/ModMiiSkin.bat
@@ -31,7 +31,7 @@ Exit
 
 ::----------------------------------------------------------
 
-set currentversion=6.4.2
+set currentversion=6.4.3
 set currentversioncopy=%currentversion%
 set agreedversion=
 
