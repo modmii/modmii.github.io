bfedd1ea7664c6b4cce9a7a2eb0dff75a8b0a245
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-11-17 09:29:14 +0000

    Update NUS_ALL.bat

--- Support/DownloadQueues/NUS_ALL.bat
+++ Support/DownloadQueues/NUS_ALL.bat
@@ -1,147 +1,65 @@
-::ModMii v6.6.4 - Download Queue - Tue 02/21/2023 -  9:03:16.78
+@echo off
+setlocal enabledelayedexpansion
+
+:: ModMii v6.6.4 - Download Queue - %DATE% - %TIME%
 set AdvNumber=0
-if /i "%GetAdvNumberOnly%" EQU "Y" goto:endofqueue
-::Queue Settings
-Set ROOTSAVE=off
-Set Option1=on
-::Queue
-SET EULAU=*
-SET EULAE=*
-SET EULAJ=*
-SET EULAK=*
-SET RSU=*
-SET RSE=*
-SET RSJ=*
-SET RSK=*
-SET BC=*
-SET SM3.2U=*
-SET SM4.1U=*
-SET SM4.2U=*
-SET SM4.3U=*
-SET SM3.2E=*
-SET SM4.1E=*
-SET SM4.2E=*
-SET SM4.3E=*
-SET SM3.2J=*
-SET SM4.1J=*
-SET SM4.2J=*
-SET SM4.3J=*
-SET SM4.1K=*
-SET SM4.2K=*
-SET SM4.3K=*
-SET IOS30=*
-SET IOS60P=*
-SET IOS70=*
-SET IOS80=*
-SET MyM=*
-SET MII=*
-SET P0=*
-SET P=*
-SET S=*
-SET SK=*
-SET IU=*
-SET IE=*
-SET IJ=*
-SET WU=*
-SET WE=*
-SET WJ=*
-SET NU=*
-SET NE=*
-SET NJ=*
-SET WSU=*
-SET WSE=*
-SET WSJ=*
-SET M10=*
-SET IOS9=*
-SET IOS12=*
-SET IOS13=*
-SET IOS14=*
-SET IOS15=*
-SET IOS17=*
-SET IOS21=*
-SET IOS22=*
-SET IOS28=*
-SET IOS31=*
-SET IOS33=*
-SET IOS34=*
-SET IOS35=*
-SET IOS36=*
-SET IOS36v3608=*
-SET IOS37=*
-SET IOS38=*
-SET IOS41=*
-SET IOS48v4124=*
-SET IOS43=*
-SET IOS45=*
-SET IOS46=*
-SET IOS53=*
-SET IOS55=*
-SET IOS56=*
-SET IOS57=*
-SET IOS58=*
-SET IOS59=*
-SET IOS61=*
-SET IOS62=*
-SET A0e=*
-SET A01=*
-SET A0e_70=*
-SET A01_60=*
-SET A0e_60=*
-SET A01_70=*
-SET A0c=*
-SET A40=*
-SET A42=*
-SET A45=*
-SET A70=*
-SET A72=*
-SET A75=*
-SET A78=*
-SET A7b=*
-SET A7e=*
-SET A84=*
-SET A87=*
-SET A8a=*
-SET A81=*
-SET A8d=*
-SET A9d=*
-SET A94=*
-SET A97=*
-SET A9a=*
-SET cIOS222[38]-v4=*
-SET cIOS223[37-38]-v4=*
-SET cIOS222[38]-v5=*
-SET cIOS223[37]-v5=*
-SET cIOS224[57]-v5=*
-SET cIOS202[60]-v5.1R=*
-SET cIOS222[38]-v5.1R=*
-SET cIOS223[37]-v5.1R=*
-SET cIOS224[57]-v5.1R=*
-SET cIOS249-v14=*
-SET cIOS249-v17b=*
-SET cIOS249[37]-v19=*
-SET cIOS249[38]-v19=*
-SET cIOS249[57]-v19=*
-SET cIOS249[38]-v20=*
-SET cIOS249[56]-v20=*
-SET cIOS249[57]-v20=*
-SET cIOS249[37]-v21=*
-SET cIOS249[38]-v21=*
-SET cIOS249[53]-v21=*
-SET cIOS249[55]-v21=*
-SET cIOS249[56]-v21=*
-SET cIOS249[57]-v21=*
-SET cIOS249[58]-v21=*
-SET cIOS249[37]-d2x-v10-beta52=*
-SET cIOS249[38]-d2x-v10-beta52=*
-SET cIOS249[56]-d2x-v10-beta52=*
-SET cIOS249[53]-d2x-v10-beta52=*
-SET cIOS249[55]-d2x-v10-beta52=*
-SET cIOS249[57]-d2x-v10-beta52=*
-SET cIOS249[58]-d2x-v10-beta52=*
-SET cIOS249[60]-d2x-v10-beta52=*
-SET cIOS249[70]-d2x-v10-beta52=*
-SET cIOS249[80]-d2x-v10-beta52=*
-SET RVL-cMIOS-v65535(v10)_WiiGator_WiiPower_v0.2=*
-SET RVL-cmios-v4_WiiGator_GCBL_v0.2=*
-SET RVL-cmios-v4_Waninkoko_rev5=*
+
+if /i "%GetAdvNumberOnly%" EQU "Y" goto :endofqueue
+
+:: Queue Settings
+set ROOTSAVE=off
+set Option1=on
+
+:: Queue
+:: Use arrays to simplify repetitive variable setting
+set "regions=U E J K"
+set "versions=3.2 4.1 4.2 4.3"
+
+for %%r in (%regions%) do (
+    set EULA%%r=*
+    set RS%%r=*
+
+    for %%v in (%versions%) do (
+        if not "%%r"=="K" if not "%%v"=="3.2" (
+            set SM%%v%%r=*
+        )
+    )
+)
+
+:: Set other variables
+set "iosList=30 60P 70 80"
+for %%i in (%iosList%) do set IOS%%i=*
+
+set "otherVars=BC MyM MII P0 P S SK IU IE IJ WU WE WJ NU NE NJ WSU WSE WSJ M10"
+for %%v in (%otherVars%) do set %%v=*
+
+:: IOS versions
+set "iosVersions=9 12 13 14 15 17 21 22 28 31 33 34 35 36 37 38 41 43 45 46 53 55 56 57 58 59 61 62"
+for %%i in (%iosVersions%) do set IOS%%i=*
+
+set IOS36v3608=*
+set IOS48v4124=*
+
+:: Additional variables  
+set "addVars=A0e A01 A0e_70 A01_60 A0e_60 A01_70 A0c A40 A42 A45 A70 A72 A75 A78 A7b A7e A84 A87 A8a A81 A8d A9d A94 A97 A9a"
+for %%v in (%addVars%) do set %%v=*
+
+:: cIOS versions
+set "cIOSVersions=cIOS222[38]-v4 cIOS223[37-38]-v4 cIOS222[38]-v5 cIOS223[37]-v5 cIOS224[57]-v5 cIOS202[60]-v5.1R cIOS222[38]-v5.1R cIOS223[37]-v5.1R cIOS224[57]-v5.1R cIOS249-v14 cIOS249-v17b"
+for %%c in (%cIOSVersions%) do set %%c=*
+
+:: cIOS249 versions
+set "cIOS249Versions=37 38 53 55 56 57 58 60 70 80"
+for %%v in (%cIOS249Versions%) do (
+    set cIOS249[%%v]-v19=*
+    set cIOS249[%%v]-v20=*
+    set cIOS249[%%v]-v21=*
+    set cIOS249[%%v]-d2x-v10-beta52=*
+)
+
+:: MIOS versions
+set RVL-cMIOS-v65535(v10)_WiiGator_WiiPower_v0.2=*
+set RVL-cmios-v4_WiiGator_GCBL_v0.2=*
+set RVL-cmios-v4_Waninkoko_rev5=*
+
 :endofqueue
