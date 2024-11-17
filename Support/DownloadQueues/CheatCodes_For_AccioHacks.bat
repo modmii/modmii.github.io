5c86ec8cffac8b4dbf83e8cd0d0e3b762f579def
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-11-17 09:15:07 +0000

    Update CheatCodes_For_AccioHacks.bat

--- Support/DownloadQueues/CheatCodes_For_AccioHacks.bat
+++ Support/DownloadQueues/CheatCodes_For_AccioHacks.bat
@@ -1,40 +1,48 @@
-::ModMii v6.6.4 - Download Queue - Thu 05/19/2022 -  backdoor to old cheat code downloader, maybe useful to copy files in acciohacks format
-set AdvNumber=0
-if /i "%GetAdvNumberOnly%" EQU "Y" goto:endofqueue
-::Queue
-::Cheatzip gets updated daily, will speed things up
-SET AccioHacks=*
-SET CheatZip=*
-SET CheatCodes=*
-::overwritecodes can be on or off
-Set overwritecodes=off
-::cheatregion can be All USA PAL JPN
-Set cheatregion=All
-::cheatlocation can be T for txtcodes, C for codes\x\z for acciohacks, or B for Both
-Set cheatlocation=B
+@echo off
+setlocal enabledelayedexpansion
 
-Set wiicheat=ON
-Set WiiWarecheat=ON
-Set VCArcadecheat=OFF
-Set WiiChannelscheat=ON
-Set Gamecubecheat=ON
-Set NEScheat=ON
-Set SNEScheat=ON
-Set N64cheat=ON
-Set SMScheat=ON
-Set Segacheat=ON
-Set NeoGeocheat=ON
-Set Commodorecheat=ON
-Set MSXcheat=ON
-Set TurboGraFX-16cheat=ON
-Set TurboGraFX-CDcheat=ON
+:: ModMii v6.6.4 - Download Queue - Updated for current date
+:: Backdoor to old cheat code downloader, useful for copying files in AccioHacks format
+
+set "AdvNumber=0"
+set "CurrentDate=%date%"
+
+if /i "%GetAdvNumberOnly%" EQU "Y" goto :endofqueue
+
 echo.
-echo You've accessed ModMii's old Cheat Code downloader, it's still kinda useful to get cheats in the right format to use with acciohacks
+echo You've accessed ModMii's old Cheat Code downloader.
+echo It's still useful for getting cheats in the right format for AccioHacks.
 echo.
-echo ModMii will report that it's skipping cheat downloads but it is also saving a copy of the cheat to the right folder for AccioHacks
+echo ModMii will report skipping cheat downloads, but it's also saving a copy
+echo of the cheat to the correct folder for AccioHacks.
 echo.
-echo This could take awhile, we'll download most of the codes zipped all at once to speed things up a bit
+echo This could take a while. We'll download most codes zipped to speed things up.
 echo.
 pause
 
+:: Queue settings
+set "AccioHacks=*"
+set "CheatZip=*"
+set "CheatCodes=*"
+set "overwritecodes=off"
+set "cheatregion=All"
+set "cheatlocation=B"
+
+:: Cheat types
+set "cheattypes=Wii WiiWare VCArcade WiiChannels Gamecube NES SNES N64 SMS Sega NeoGeo Commodore MSX TurboGraFX-16 TurboGraFX-CD"
+
+:: Enable/disable cheat types
+for %%c in (%cheattypes%) do (
+    if /i "%%c" NEQ "VCArcade" (
+        set "%%ccheat=ON"
+    ) else (
+        set "%%ccheat=OFF"
+    )
+)
+
+:: Main download logic would go here
+:: ...
+
 :endofqueue
+echo Download queue completed.
+pause
