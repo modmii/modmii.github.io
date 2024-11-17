eac9abbf0c36886287876ca867a96655bf3b55bc
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-11-17 09:59:48 +0000

    Update TMCL_Portable_Fix_Reversal.bat

--- Support/subscripts/TMCL_Portable_Fix_Reversal.bat
+++ Support/subscripts/TMCL_Portable_Fix_Reversal.bat
@@ -1,14 +1,30 @@
-::Portable ModMii Installation fix, reversal
-if /i "%Homedrive%" EQU "%ModMiiDrive%" goto:skipPortableFix
-if exist "%homedrive%\ModMii\temp\TMCL.exe" del "%homedrive%\ModMii\temp\TMCL.exe">nul
-if exist "%homedrive%\ModMii\temp\ASH.exe" del "%homedrive%\ModMii\temp\ASH.exe">nul
-if exist "%homedrive%\ModMii\temp\ICSharpCode.SharpZipLib.dll" del "%homedrive%\ModMii\temp\ICSharpCode.SharpZipLib.dll">nul
-if exist "%homedrive%\ModMii\temp\themewii.exe" del "%homedrive%\ModMii\temp\themewii.exe">nul
+@echo off
+setlocal enabledelayedexpansion
 
-::check for and delete empty folders
-dir /A /B "%homedrive%\ModMii\temp" | findstr /R ".">NUL && goto:notempty
-rd /s /q "%homedrive%\ModMii\temp"> nul
-:notempty
-dir /A /B "%homedrive%\ModMii" | findstr /R ".">NUL && goto:skipPortableFix
-rd /s /q "%homedrive%\ModMii"> nul
+:: Portable ModMii Installation fix and reversal
+if /i "%Homedrive%" EQU "%ModMiiDrive%" goto :skipPortableFix
+
+:: Delete specific files in the temp directory
+set "tempDir=%homedrive%\ModMii\temp"
+set "filesToDelete=TMCL.exe ASH.exe ICSharpCode.SharpZipLib.dll themewii.exe"
+
+for %%f in (%filesToDelete%) do (
+    if exist "!tempDir!\%%f" del "!tempDir!\%%f" >nul 2>&1
+)
+
+:: Check for and delete empty folders
+call :deleteIfEmpty "%homedrive%\ModMii\temp"
+call :deleteIfEmpty "%homedrive%\ModMii"
+
+goto :skipPortableFix
+
+:deleteIfEmpty
+if exist "%~1" (
+    dir /A /B "%~1" 2>nul | findstr "^" >nul || rd /s /q "%~1" >nul 2>&1
+)
+goto :eof
+
 :skipPortableFix
+:: Continue with the rest of your script here
+
+endlocal
